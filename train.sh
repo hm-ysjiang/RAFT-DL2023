41bbb3588303fbebb64d939005b2c838968c5a30
hm-ysjiang <hmysjiang.git@gmail.com>
2023-06-07 13:53:46 +0800

    Training Commands

---
+++ train.sh
@@ -0,0 +1,48 @@
+# From scratch
+cmd_supervised_scratch="python -u train-supervised.py \
+                               --name supervised-scratch-c128 \
+                               --validation sintel \
+                               --num_epochs 100 \
+                               --batch_size 6 \
+                               --lr 0.0004 \
+                               --wdecay 0.00001 \
+                               --context 128"
+
+cmd_selfsupervised_scratch="python -u train-selfsupervised.py \
+                                   --name selfsupervised-scratch-c128 \
+                                   --validation sintel \
+                                   --num_epochs 100 \
+                                   --batch_size 6 \
+                                   --lr 0.0004 \
+                                   --wdecay 0.00001 \
+                                   --context 128"
+
+# Transfer
+cmd_supervised_transfer="python -u train-supervised.py \
+                                --name supervised-transfer-c128 \
+                                --validation sintel \
+                                --restore_ckpt checkpoints/raft-things.pth \
+                                --num_epochs 100 \
+                                --batch_size 6 \
+                                --lr 0.0002 \
+                                --wdecay 0.00001 \
+                                --gamma=0.85 \
+                                --reset_context \
+                                --context 128"
+
+cmd_selfsupervised_transfer="python -u train-selfsupervised.py \
+                                    --name selfsupervised-transfer-c128 \
+                                    --validation sintel \
+                                    --restore_ckpt checkpoints/raft-things.pth \
+                                    --num_epochs 100 \
+                                    --batch_size 6 \
+                                    --lr 0.0002 \
+                                    --wdecay 0.00001 \
+                                    --gamma=0.85 \
+                                    --reset_context \
+                                    --context 128"
+
+cmd=$cmd_supervised_transfer    # Change this line
+
+echo ${cmd}
+eval ${cmd}
