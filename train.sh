e10af9b196f7b0a5b7ffc65188ff09c3b4418ef4
hm-ysjiang <hmysjiang.git@gmail.com>
2023-06-07 14:03:59 +0800

    Unify batch size

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ cmd_supervised_scratch="python -u train-supervised.py \
                                --name supervised-scratch-c128 \
                                --validation sintel \
                                --num_epochs 100 \
-                               --batch_size 6 \
+                               --batch_size 3 \
                                --lr 0.0004 \
                                --wdecay 0.00001 \
                                --context 128"
@@ -12,7 +12,7 @@ cmd_selfsupervised_scratch="python -u train-selfsupervised.py \
                                    --name selfsupervised-scratch-c128 \
                                    --validation sintel \
                                    --num_epochs 100 \
-                                   --batch_size 6 \
+                                   --batch_size 3 \
                                    --lr 0.0004 \
                                    --wdecay 0.00001 \
                                    --context 128"
@@ -23,7 +23,7 @@ cmd_supervised_transfer="python -u train-supervised.py \
                                 --validation sintel \
                                 --restore_ckpt checkpoints/raft-things.pth \
                                 --num_epochs 100 \
-                                --batch_size 6 \
+                                --batch_size 3 \
                                 --lr 0.0002 \
                                 --wdecay 0.00001 \
                                 --gamma=0.85 \
@@ -36,7 +36,7 @@ cmd_selfsupervised_transfer="python -u train-selfsupervised.py \
                                     --validation sintel \
                                     --restore_ckpt checkpoints/raft-things.pth \
                                     --num_epochs 100 \
-                                    --batch_size 6 \
+                                    --batch_size 3 \
                                     --lr 0.0002 \
                                     --wdecay 0.00001 \
                                     --gamma=0.85 \
